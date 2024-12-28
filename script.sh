43e8516bbacced8da4fbecb6288e068a24f7f136
ankur0000001 <ankur000001@gmail.com>
2024-12-28 14:39:09 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,19 @@
 set -ex
 
+
+sudo apt-get update
+sudo apt-get install -y flex
+
+mkdir ~/workspace
+cd ~/workspace
+
 wget -q 'https://drive.usercontent.google.com/download?id=1c44rqqZDw6-jJDKpgNHCJBopUiHFAXZG&export=download&confirm=t' -O sourcecode.zip
 unzip sourcecode.zip
 tar -xzf Kernel.tar.gz
 cd kernel-5.10
+
 git clone https://gitlab.com/kalilinux/nethunter/build-scripts/kali-nethunter-kernel-builder
 cd kali-nethunter-kernel-builder/
-sudo apt-get update
-sudo apt-get install -y flex
 
 cat <<'EOF' > local.config
 CROSS_COMPILE_SRC="https://kali.download/nethunter-images/toolchains/google_aarch64-standalone-oreo-4.9.tar.xz"
