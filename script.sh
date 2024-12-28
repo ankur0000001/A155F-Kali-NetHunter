6a5db8424fb4910b340ca2a92343e17c59b9f52d
ankur0000001 <ankur000001@gmail.com>
2024-12-28 06:51:26 +0000

    script updated

--- script.sh
+++ script.sh
@@ -1,7 +1,11 @@
-wget -q 'https://drive.usercontent.google.com/download?id=1NNeoUX6T4UMeodwbhA02teTFyzuUmNlO&export=download&confirm=t' -O sourcecode.zip
+set -ex
+
+wget -q 'https://drive.usercontent.google.com/download?id=1c44rqqZDw6-jJDKpgNHCJBopUiHFAXZG&export=download&confirm=t' -O sourcecode.zip
 unzip sourcecode.zip
-tar -xf Kernel.tar.xz
+tar -xzf Kernel.tar.gz
 cd kernel-5.10
 git clone https://gitlab.com/kalilinux/nethunter/build-scripts/kali-nethunter-kernel-builder
 cd kali-nethunter-kernel-builder/
+sudo apt-get update
+sudo apt-get install -y flex
 ./build.sh
